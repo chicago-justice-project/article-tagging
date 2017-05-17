b9310ccdb852b74872606216571a98d528db83b5
Kevin Rose <kevin+gh@maypark.com>
2017-05-16 20:53:45 -0500

    adding initial investigation into direct crime linking (#14)

--- src/load_data.py
+++ src/load_data.py
@@ -72,3 +72,43 @@ def load_data():
         df.loc[row['article_id'], row['category_abbreviation']] = 1
 
     return df
+
+
+def load_crime_data():
+    crimes = pd.read_csv(os.path.join(DATA_FOLDER, 'Crimes.csv'))
+    crimes = crimes[crimes['Year'] > 2010]
+
+    crime_string = pd.Series('', crimes.index)
+
+    # ['ID', 'Case Number', 'Date', 'Block', 'IUCR', 'Primary Type',
+    #  'Description', 'Location Description', 'Arrest', 'Domestic', 'Beat',
+    #  'District', 'Ward', 'Community Area', 'FBI Code', 'X Coordinate',
+    #  'Y Coordinate', 'Year', 'Updated On', 'Latitude', 'Longitude',
+    #  'Location']
+
+    # TODO: synonyms on this for month name, weekday name, time of day (e.g. afternoon), etc.
+    crime_string += crimes['Date'] + ' '
+
+    # TODO: synonyms?
+    crime_string += crimes['Primary Type'] + ' '
+
+    # TODO: synonyms?
+    crime_string += crimes['Description'] + ' '
+
+    # TODO: synonyms?
+    crime_string += crimes['Location Description'] + ' '
+
+    # TODO: synonyms?
+    iucr = pd.read_csv(os.path.join(DATA_FOLDER, 'IUCR.csv'))
+    iucr.set_index('IUCR', drop=True, inplace=True)
+    idx = iucr.index
+    idx_values = idx.values
+    idx_values[idx.str.len() == 3] = '0' + idx_values[idx.str.len() == 3]
+    crime_string += iucr.loc[crimes['IUCR'], 'PRIMARY DESCRIPTION'].fillna('').values + ' '
+    crime_string += iucr.loc[crimes['IUCR'], 'SECONDARY DESCRIPTION'].fillna('').values + ' '
+
+    community_areas = pd.read_csv(os.path.join(DATA_FOLDER, 'CommAreas.csv'))
+    community_areas.set_index('AREA_NUM_1', inplace=True, drop=True)
+    crime_string += community_areas.loc[crimes['Community Area'], 'COMMUNITY'].fillna('').values + ' '
+
+    return crimes, crime_string
