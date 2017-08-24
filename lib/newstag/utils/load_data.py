a0ab5a815a67df697a23d37e262e830870eda770
Kevin Rose <kevin+gh@maypark.com>
2017-08-23 23:36:11 -0500

    updates to loading

--- lib/newstag/utils/load_data.py
+++ lib/newstag/utils/load_data.py
@@ -1,64 +1,88 @@
 import pandas as pd
+import numpy as np
 import os
 
-DATA_FOLDER = os.path.join(os.path.split(__file__)[0], '../data/')
+"""
+Helper functions to load the article data. The main method to use
+is load_data().
+"""
 
-def load_articles():
-    """Loads the articles CSV."""
+# Caution! Modifying this in code will have no effect since the
+# default arguments are populated with this reference at creation
+# time, so post-hoc modifications will do nothing.
+__data_folder = os.path.join(os.path.split(__file__)[0], '..', 'data')
+
+def load_articles(data_folder=__data_folder, nrows=None):
+    """
+    Loads the articles CSV. Can optionally only load the first
+    `nrows` number of rows.
+    """
     column_names = ['id',
-                    'feedname',
+                    'unknown1',
                     'url',
                     'orig_html',
                     'title',
                     'bodytext',
                     'relevant',
                     'created',
-                    'last_modified']
+                    'last_modified',
+                    'unknown2',
+                    'feedname']
 
-    return pd.read_csv(os.path.join(DATA_FOLDER,
+    return pd.read_csv(os.path.join(data_folder,
                                     'newsarticles_article.csv'),
                        header=None,
-                       names=column_names)
+                       names=column_names,
+                       nrows=nrows)
 
 
-def load_categorizations():
+def load_categorizations(data_folder=__data_folder):
     """Loads the categorizations of the articles."""
     column_names = ['id', 'article_id', 'category_id']
 
-    return pd.read_csv(os.path.join(DATA_FOLDER,
+    return pd.read_csv(os.path.join(data_folder,
                                     'newsarticles_article_categories.csv'),
                        header=None,
                        names=column_names)
 
 
-def load_categories():
+def load_categories(data_folder=__data_folder):
     """Loads the mapping of id to names/abbrevations of categories"""
     column_names = ['id', 'category_name', 'abbreviation', 'created',
                     'unknown', 'group']
 
-    return pd.read_csv(os.path.join(DATA_FOLDER, 'newsarticles_category.csv'),
+    return pd.read_csv(os.path.join(data_folder, 'newsarticles_category.csv'),
                        header=None,
                        names=column_names)
 
 
-def load_data():
-    """Creates a dataframe of the article information and k-hot encodes the tags
+def load_data(data_folder=__data_folder, nrows=None):
+    """
+    Creates a dataframe of the article information and k-hot encodes the tags
     into columns called cat_NUMBER. The k-hot encoding is done assuming that the
     categories are 1-indexed and there are as many categories as the maximum
-    value of the numerical cateogry_id column."""
+    value of the numerical cateogry_id column.
 
-    df = load_articles()
+    Inputs:
+        data_folder:
+            A folder containing the data files in CSV format.
+        nrows:
+            Number of articles to load. Defaults to all, which uses about 4
+            GB of memory.
+    """
+
+    df = load_articles(data_folder=data_folder, nrows=nrows)
     df['relevant'] = df['relevant'] == 't'
     df.rename(columns={'id': 'article_id'}, inplace=True)
     df.set_index('article_id', drop=True, inplace=True)
     # hopefully this will save some memory/space, can add back if needed
     del(df['orig_html'])
 
-    tags_df = load_categorizations()
+    tags_df = load_categorizations(data_folder)
     # will help cacheing
     tags_df.sort_values(by='article_id', inplace=True)
 
-    categories_df = load_categories()
+    categories_df = load_categories(data_folder)
     categories_df.set_index('id', drop=True, inplace=True)
 
     for i in range(tags_df['category_id'].max()):
@@ -68,15 +92,24 @@ def load_data():
         df[cat_name] = df[cat_name].astype('int8') # save on that memory!
 
     # tags_df['category_id'] = tags_df['category_id'].astype(str)
-    tags_df['category_abbreviation'] = categories_df['abbreviation'][tags_df['category_id']].values
-    for _, row in tags_df.iterrows():
-        df.loc[row['article_id'], row['category_abbreviation']] = 1
+    tags_df['category_abbreviation'] = (categories_df
+                                        ['abbreviation']
+                                        [tags_df['category_id']]
+                                        .values)
+
+    if np.setdiff1d(tags_df['article_id'].values, df.index.values).size:
+        print('Warning, tags were found for article IDs that do not exist.')
+
+    article_ids = tags_df['article_id'].values
+    cat_abbreviations = tags_df['category_abbreviation'].values
+    existing_ids_filter = np.isin(article_ids, df.index.values)
+    df.loc[article_ids[existing_ids_filter], cat_abbreviations[existing_ids_filter]] = 1
 
     return df
 
 
-def load_crime_data():
-    crimes = pd.read_csv(os.path.join(DATA_FOLDER, 'Crimes.csv'))
+def load_crime_data(data_folder=__data_folder):
+    crimes = pd.read_csv(os.path.join(data_folder, 'Crimes.csv'))
     crimes = crimes[crimes['Year'] > 2010]
 
     crime_string = pd.Series('', crimes.index)
@@ -87,7 +120,8 @@ def load_crime_data():
     #  'Y Coordinate', 'Year', 'Updated On', 'Latitude', 'Longitude',
     #  'Location']
 
-    # TODO: synonyms on this for month name, weekday name, time of day (e.g. afternoon), etc.
+    # TODO: synonyms on this for month name, weekday name,
+    # time of day (e.g. afternoon), etc.
     crime_string += crimes['Date'] + ' '
 
     # TODO: synonyms?
@@ -100,16 +134,25 @@ def load_crime_data():
     crime_string += crimes['Location Description'] + ' '
 
     # TODO: synonyms?
-    iucr = pd.read_csv(os.path.join(DATA_FOLDER, 'IUCR.csv'))
+    iucr = pd.read_csv(os.path.join(data_folder, 'IUCR.csv'))
     iucr.set_index('IUCR', drop=True, inplace=True)
     idx = iucr.index
     idx_values = idx.values
     idx_values[idx.str.len() == 3] = '0' + idx_values[idx.str.len() == 3]
-    crime_string += iucr.loc[crimes['IUCR'], 'PRIMARY DESCRIPTION'].fillna('').values + ' '
-    crime_string += iucr.loc[crimes['IUCR'], 'SECONDARY DESCRIPTION'].fillna('').values + ' '
-
-    community_areas = pd.read_csv(os.path.join(DATA_FOLDER, 'CommAreas.csv'))
+    crime_string += (iucr.loc[crimes['IUCR'], 'PRIMARY DESCRIPTION']
+                     .fillna('')
+                     .values
+                     + ' ')
+    crime_string += (iucr.loc[crimes['IUCR'], 'SECONDARY DESCRIPTION']
+                     .fillna('')
+                     .values
+                     + ' '
+)
+    community_areas = pd.read_csv(os.path.join(data_folder, 'CommAreas.csv'))
     community_areas.set_index('AREA_NUM_1', inplace=True, drop=True)
-    crime_string += community_areas.loc[crimes['Community Area'], 'COMMUNITY'].fillna('').values + ' '
+    crime_string += (community_areas.loc[crimes['Community Area'], 'COMMUNITY']
+                     .fillna('')
+                     .values
+                     + ' ')
 
     return crimes, crime_string
