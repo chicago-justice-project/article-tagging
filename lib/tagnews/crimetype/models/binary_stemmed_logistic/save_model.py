0d607f6af7b54517bb6b5df2be76ae9ff74cfa57
Kevin Rose <kevin+gh@maypark.com>
2017-08-28 23:55:57 -0500

    reproducible, py27 compataible model saving

--- lib/tagnews/crimetype/models/binary_stemmed_logistic/save_model.py
+++ lib/tagnews/crimetype/models/binary_stemmed_logistic/save_model.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 from ....utils import load_data as ld
 from ....utils.model_helpers import LemmaTokenizer
 import matplotlib
@@ -11,33 +14,39 @@ import sklearn.linear_model
 from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
+np.random.seed(1029384756)
+
 df = ld.load_data()
 
 crime_df = df.ix[df.loc[:, 'OEMC':'TASR'].any(1), :]
 crime_df = crime_df.append(df.ix[~df['relevant'], :].sample(n=3000, axis=0))
 
-idx = np.random.permutation(crime_df.shape[0])
-trn = crime_df.iloc[idx[:int(crime_df.shape[0] * 0.7)], :]
-tst = crime_df.iloc[idx[int(crime_df.shape[0] * 0.7):], :]
-
 vectorizer = sklearn.feature_extraction.text.CountVectorizer(tokenizer=LemmaTokenizer(),
                                                              binary=True)
-X = vectorizer.fit_transform(trn['bodytext'].values)
-
-Y = trn.loc[:, 'OEMC':'TASR'].values
 
 clf = sklearn.multiclass.OneVsRestClassifier(
     sklearn.linear_model.LogisticRegression()
 )
 
-X = vectorizer.transform(crime_df['bodytext'].values)
+X = vectorizer.fit_transform(crime_df['bodytext'].values)
 Y = crime_df.loc[:, 'OEMC':'TASR'].values
 
 clf.fit(X, Y)
 
+from ...tag import Tagger
+
+tagger = Tagger(clf=clf, vectorizer=vectorizer)
+
+print(tagger.tagtext_proba(('This is an article about drugs and'
+                            ' gangs. Copyright Kevin Rose.')))
+
 import pickle
 
-with open('model.pkl', 'wb') as f:
+curr_time = time.strftime("%Y%m%d-%H%M%S")
+
+with open(os.path.join(os.path.split(__file__)[0],
+                       'model-' + curr_time + '.pkl'), 'wb') as f:
     pickle.dump(clf, f)
-with open('vectorizer.pkl', 'wb') as f:
+with open(os.path.join(os.path.split(__file__)[0],
+                       'vectorizer-' + curr_time + '.pkl'), 'wb') as f:
     pickle.dump(vectorizer, f)
