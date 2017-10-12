ff4dc8c82db3eed42b5ccb8a149513a87ffcaefc
Kevin Rose <kevin+gh@maypark.com>
2017-10-12 00:19:48 -0500

    more micro-optimizations on loading

--- lib/tagnews/utils/load_vectorizer.py
+++ lib/tagnews/utils/load_vectorizer.py
@@ -15,29 +15,13 @@ def load_glove(vectors_file, normalize=True):
     >>> text = 'This is a sentence and stuff.'
     >>> vectorized_text = vectors.loc[[word.lower() for word in text.split()]]
     >>> print(vectorized_text.shape)
-        (300, 6)
+        (6, 300)
     >>> k = 5
     >>> import numpy as np
     >>> def euc(word):
     ...     return np.sum((vectors.values - vectors.loc[word].values) ** 2.0, 1)
+    ...
     >>> vectors.index[np.argpartition(euc('murder'), range(k))[:k]]
-
-import tagnews
-import datetime
-print(datetime.datetime.now())
-vectors = tagnews.utils.load_vectorizer.load_glove('tagnews/data/glove.840B.300d.txt', normalize=False)
-print(datetime.datetime.now())
-text = 'This is a sentence and stuff.'
-vectorized_text = vectors.loc[[word.lower() for word in text.split()]]
-print(vectorized_text.shape)
-k = 5
-import numpy as np
-def euc(word):
-    return np.sum((vectors.values - vectors.loc[word].values) ** 2.0, 1)
-
-vectors.index[np.argpartition(euc('murder'), range(k))[:k]]
-
-Index(['murder', 'murders', 'murdered', 'homicide', 'crime'], dtype='object')
 
     Inputs:
         vectors_file: path to file that contains GloVe formatted word
@@ -50,40 +34,21 @@ Index(['murder', 'murders', 'murdered', 'homicide', 'crime'], dtype='object')
         vectors: NxM pandas dataframe whose rows are indexed by the word.
     """
 
-    vocab = {}
-    duplicate_word_idxs = []
-    num_duplicates = 0
+    with open(vectors_file, 'r', encoding='utf-8') as f:
+        for vocab_size, line in enumerate(f):
+            pass
+    vocab_size += 1
+
+    vec_size = len(line.split(' ')) - 1
+    vectors = np.zeros((vocab_size, vec_size), dtype=np.float32)
+    words = np.empty(shape=(vocab_size), dtype=np.dtype('object'))
 
     with open(vectors_file, 'r', encoding='utf-8') as f:
         for i, line in enumerate(f):
-            word = line[:line.index(' ')]
-            if word in vocab:
-                duplicate_word_idxs.append(i)
-                num_duplicates += 1
-                continue
-            vocab[word] = i - num_duplicates
+            line = line.split(' ')
+            words[i] = line[0]
+            vectors[i] = [float(x) for x in line[1:]]
 
-    # As written, this is guaranteed to be sorted. But sorting
-    # this will be cheap and good future proofing.
-    duplicate_word_idxs = sorted(duplicate_word_idxs)
-    skip_counter = 0
-    vec_size = len(line.rstrip().split(' ')) - 1
-    w = np.zeros((len(vocab), vec_size), dtype=np.float32)
+    vectors = pd.DataFrame(vectors, index=words, copy=False)
 
-    with open(vectors_file, 'r', encoding='utf-8') as f:
-        for i, line in enumerate(f):
-            if skip_counter < num_duplicates and i == duplicate_word_idxs[skip_counter]:
-                skip_counter += 1
-                continue
-            w[i - skip_counter] = [float(x) for x in line.split(' ')[1:]]
-
-    if normalize:
-        w = sklearn.preprocessing.normalize(w)
-
-    vectors = pd.DataFrame(
-        w,
-        index=[item[0] for item in sorted(vocab.items(), key=lambda item: item[1])],
-        copy=False
-    )
-
-    return vectors
+    return vectors.loc[~vectors.index.duplicated()]
