0412aef653ceda6223a74ef28efcfd8ab3d82c02
perfecthair <kinstler.john@gmail.com>
2017-10-15 16:03:56 -0500

    feat: added geotagging of addresses rewrote geocode.ipynb to include the article id into the address list which also required rewriting lat_long.py a bit added addr_geotag_list.pkl file as output from geocode that will then become input to geotag load gmaps and folium packages along with nodejs and ipywidgets to display maps inside the notebook geotag additionally created a yaml file to create the environment with all necessarypackages to run these notebooks :

--- lib/notebooks/mapper.py
+++ lib/notebooks/mapper.py
@@ -1,3 +1,11 @@
 
-
-baseURL = 'https://maps.googleapis.com/maps/api/staticmap?center=Chicago,IL&zoom=12&size=640x640&key='
+def mapper(loclist, api_key):
+    baseURL = 'http://maps.googleapis.com/maps/api/staticmap?center=Chicago,IL&zoom=11&size=640x640&scale=2'
+    
+    markers = '&markers=size:mid&color:red'
+    for i in loclist:
+        for j in i:
+            if 'lat_long' in j.keys():
+                markers += '%7C' + str(j['lat_long'][1][0]) + ',' + str(j['lat_long'][1][1])
+    
+    return baseURL + markers + '&key=' + api_key
