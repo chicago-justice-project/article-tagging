290a1b106d41956129c8086feae5d5385bf3aeda
jherzberg <jlherzberg@gmail.com>
2018-02-13 21:10:49 -0600

    multiple providers

--- lib/tagnews/utils/geocode_list.py
+++ lib/tagnews/utils/geocode_list.py
@@ -11,3 +11,18 @@ def lat_longs_from_geo_strings(lst):
         else:
             lats_lons.append(g.latlng)
     return lats_lons
+
+def multi_option_lat_longs(lst, provider='arcgis'):
+    providers = ['arcgis', 'google', 'yandex', 'geocodefarm', 'osm']
+    assert provider in providers, \
+        'I\'m sorry Dave, I\'m afraid I can\'t do that. \
+        Please choose a provider from {}!'.format(' or '.join(providers))
+    lats_lons = []
+    for addr_str in lst:
+        g = getattr(geocoder, provider)(addr_str)
+        if g.latlng is None:
+            time.sleep(.5)
+            lats_lons.extend(lat_longs_from_geo_strings([addr_str], provider))
+        else:
+            lats_lons.append(g.latlng)
+    return lats_lons
