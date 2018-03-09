d7fdf77475f34e740e7ee172027a6a1282e3d052
Kevin Rose <kevin+gh@maypark.com>
2018-03-08 20:49:50 -0600

    Version bump.

--- lib/tagnews/__init__.py
+++ lib/tagnews/__init__.py
@@ -7,7 +7,7 @@ from .utils.load_data import load_data
 from .utils.load_data import load_ner_data
 from .utils.load_vectorizer import load_glove
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 def test(verbosity=None, **kwargs):
     """run the test suite"""
