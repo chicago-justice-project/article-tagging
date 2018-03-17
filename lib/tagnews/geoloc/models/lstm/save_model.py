e0bc7a1e14695faca0eecbd5df7967daed22a7e5
Kevin Rose <kevin+gh@maypark.com>
2018-03-17 00:15:46 -0500

    Add ability to specify number of training epochs for geostring extractor

--- lib/tagnews/geoloc/models/lstm/save_model.py
+++ lib/tagnews/geoloc/models/lstm/save_model.py
@@ -15,6 +15,12 @@ if saved_files:
         print('Exiting.')
         exit()
 
+import sys
+if len(sys.argv) == 1:
+    num_epochs = 20
+else:
+    num_epochs = int(sys.argv[1])
+
 
 from .... import utils
 import pandas as pd
@@ -116,7 +122,7 @@ class OurAUC(keras.callbacks.Callback):
 our_auc = OurAUC()
 
 model.fit(x_train, y_train,
-          epochs=20,
+          epochs=num_epochs,
           validation_data=(x_val, y_val),
           callbacks=[our_auc, checkpointer],
           verbose=2)
