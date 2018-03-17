1753b479710cd634d059723dc109769737d172e5
Kevin Rose <kevin+gh@maypark.com>
2018-03-17 01:19:33 -0500

    Remove unused test function at top level.

--- lib/tagnews/__init__.py
+++ lib/tagnews/__init__.py
@@ -8,18 +8,3 @@ from .utils.load_data import load_ner_data
 from .utils.load_vectorizer import load_glove
 
 __version__ = '1.0.2'
-
-def test(verbosity=None, **kwargs):
-    """run the test suite"""
-
-    import pytest
-
-    args = kwargs.pop('argv', [])
-
-    if verbosity:
-        args += ['-' + 'v' * verbosity]
-
-    return pytest.main(args, **kwargs)
-
-
-test.__test__ = False  # pytest: this function is not a test
