f6976d738b567afe186915f2e5a998454851dd4f
josh herzberg <jlherzberg@gmail.com>
2020-01-14 20:38:47 -0600

    imports

--- lib/tagnews/__init__.py
+++ lib/tagnews/__init__.py
@@ -2,13 +2,12 @@ from . import utils
 from . import crimetype
 
 from .crimetype.tag import CrimeTags
+from .senteval.eval import SentimentGoogler
 from .geoloc.tag import GeoCoder, get_lat_longs_from_geostrings
 from .utils.load_data import load_data
 from .utils.load_vectorizer import load_glove
-from .senteval.eval import SentimentGoogler
-from .senteval.police_words import police_words_list, bins
 
 __all__ = [utils, crimetype, CrimeTags, GeoCoder, SentimentGoogler,
-           get_lat_longs_from_geostrings, load_data, load_glove, police_words_list, bins]
+           get_lat_longs_from_geostrings, load_data, load_glove]
 
-__version__ = '1.2.5'
+__version__ = '1.3.0'
